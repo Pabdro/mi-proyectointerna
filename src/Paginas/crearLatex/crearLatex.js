2601e5ead6284758f3a4ba346e46a9ea392aeb0b
Pabdro <pablis.badani@gmail.com>
2023-05-11 22:29:28 -0400

    barra de eleccion

--- src/Paginas/crearLatex/crearLatex.js
+++ src/Paginas/crearLatex/crearLatex.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './crearLatex.css';
-
+import { useState } from 'react';
 const CrearLatex = () => {
+    const [active, setActive] = useState(false);
+    const handleClick = () => {
+    setActive(!active);
+    };
+    const [active2, setActive2] = useState(false);
+    const handleClick2 = () => {
+        setActive2(!active2);
+        };
     return (
     <div className="CrearLatex">
-        <h2>holaaaa</h2>
+        <div class="button-bar">
+        <button className={`toggle-button ${active ? 'active' : ''}`}onClick={handleClick}>Derivadas</button>
+        <button className={`toggle-button ${active2 ? 'active' : ''}`}onClick={handleClick2}>Integrales</button>
+        </div>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
     </div>
     )
 }
